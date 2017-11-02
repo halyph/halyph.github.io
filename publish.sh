b2f7c36446c9a786b09c09e7929ce76da34061f2
Orest Ivasiv <halyph@gmail.com>
2017-11-02 21:16:55 +0100

    Updated publish script

--- publish.sh
+++ publish.sh
@@ -6,10 +6,6 @@ cp -R output/* site/
 
 SITE_DIR=site
 
-git -C $SITE_DIR status
-
-git -C $SITE_DIR diff
-
-# git -C $SITE_DIR add .
-# git -C $SITE_DIR commit -m "Site updated"
-#git -C $SITE_DIR push origin master
+git -C $SITE_DIR add .
+git -C $SITE_DIR commit -m "Site updated"
+git -C $SITE_DIR push origin master
