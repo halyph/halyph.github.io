515e012e1a385f5c6f7ee62abff5e306f41111c6
Orest Ivasiv <halyph@gmail.com>
2017-11-05 18:58:59 +0100

    updated publish script

--- publish.sh
+++ publish.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-jbake -b
-git checkout master
-cp -R output/* site/
-
 SITE_DIR=site
+jbake -b
+
+echo Publishing ...
+
+git -C $SITE_DIR pull origin
+cp -R output/* $SITE_DIR/
 
 git -C $SITE_DIR add .
 git -C $SITE_DIR commit -m "Site updated"
